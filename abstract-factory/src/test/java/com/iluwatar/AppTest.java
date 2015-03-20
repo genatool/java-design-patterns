c0554266fd0145e1b26453df138f024b4816085b
genatool <jgenarotool@gmail.com>
2015-03-20 14:49:58 -0500

    my new commit 3/20/15 -1

--- abstract-factory/src/test/java/com/iluwatar/AppTest.java
+++ abstract-factory/src/test/java/com/iluwatar/AppTest.java
@@ -25,12 +25,5 @@ public class AppTest {
         int c = b;
         App.main(args);
     }
-    public void test3() {
-        String[] args = {};
-        int a;
-        String test;
-        int b =10;
-        int c = b;
-        App.main(args);
-    }
+
 }
